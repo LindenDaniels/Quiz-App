3b269e17b1b3f82cb4c446ce5f6d3d788a2838da
Linden Daniels <lindendaniels@gmail.com>
2019-06-01 19:13:58 -0400

    another test

---
+++ quiz-store.js
@@ -0,0 +1,12 @@
+const quizData = [
+    {
+        Question: 'What was Daenerys\' three dragons\' names?',
+        Answers: [
+            'Jon, Robb, Bran',
+            'Viserys, Khal Drogo, Khaleesi',
+            'Drogon, Rhaegal, Viserion',
+            'Dracarys, Morghulis, Dohaeris'
+        ],
+        correctAnswer: 'Drogon, Rhaegal, Viserion'
+    }
+]
