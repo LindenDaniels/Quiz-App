ffdfeb5bf355cc76983ec69d7faa76623336e1d8
Linden Daniels <lindendaniels@gmail.com>
2019-05-31 23:55:26 -0400

    added function stubs

---
+++ index.js
@@ -0,0 +1,67 @@
+function handleHomePageReturn() {
+    //this function allows the user to click "turn back"
+    // to return to the beginning of the quiz
+    console.log('`handleHomePageReturn ran`');
+}
+
+function startQuiz() {
+    //this function allows the user to start the quiz
+    console.log('`startQuiz ran`');
+}
+
+function nextQuestion() {
+    //this function will advance the user to the next question
+    console.log('`nextQuestion ran`');
+}
+
+function currentQuestion() {
+    //this function will show the user which question they're on
+    console.log('currentQuestion ran`');
+}
+
+function handleUserAnswer() {
+    //this function detects if the user answered correctly or incorrectly
+    console.log('`handleUserAnswer ran`');
+}
+
+function correctAnswer() {
+    //this function displays text if the user got the answer right
+    console.log('`correctAnswer ran`');
+}
+
+function incorrectAnswer() {
+    //this function displays text if the user got the answer wrong
+    console.log('`incorrectAnswer ran`');
+}
+function handleUserScore() {
+    //this function will advance the user's score
+    console.log('`handleUserScore ran`');
+
+}
+
+function displayUserResults() {
+    //this function will tell the user if they're right or wrong with their answer
+    //and display the correct answer if they're wrong
+    console.log('`handleUserScore ran`');
+}
+
+function displayUserScore() {
+    //this function displays the user's score
+    console.log('`displayUserScore ran`');
+}
+
+function handlePictureDisplay() {
+    //this function will display different pictures based on
+    //if the user got the question right or wrong
+    console.log('`handlePictureDisplay ran`');
+}
+
+function showTotalScore() {
+    //this function will display the user's total score at the end of the quiz
+    console.log('`showTotalScore ran`');
+}
+
+function startNewQuiz() {
+    //this function will allow the user to start a new quiz
+    console.log('`startNewQuiz ran`');
+}
