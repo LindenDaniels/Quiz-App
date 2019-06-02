8216b455e5eb47a3cfb4d3029d75d2994061a7c4
Linden Daniels <lindendaniels@gmail.com>
2019-06-02 14:36:06 -0400

    filled in remaining questions

--- quizstore.js
+++ quizstore.js
@@ -8,5 +8,97 @@ const quizData = [
             'Dracarys, Morghulis, Dohaeris'
         ],
         correctAnswer: 'Drogon, Rhaegal, Viserion'
+    },
+     { 
+        question: 'Which Game of Thrones character takes the crown at the end of Season 8?',
+        answers: [
+            'Jon Snow',
+            'Daenerys Targaryen',
+            'Sansa Stark',
+            'Bran Stark'
+        ],
+        correctAnswer: 'Bran Stark'
+    },
+    {   
+        question: 'Who was blamed for Joffrey’s death at The Purple Wedding?',
+        answers: [
+            'Tyrion Lannister',
+            'Sansa Stark',
+            'Margaery Tyrell',
+            'Olenna Tyrell'
+
+        ],
+        correctAnswer: 'Olenna Tyrell'
+    },
+    {
+        question: 'What book series is the TV show Game of Thrones is based on?',
+        answers: [
+            'A Song of Ice and Fire',
+            'The Hobbit',
+            'Harry Potter',
+            'The Lord of the Rings'
+        ],
+        correctAnswer: 'A Song of Ice and Fire'
+    },
+    {  question: 'What do we say to the God of Death?',
+       answers: [
+           'Not today.',
+           'Today.',
+           'Tomorrow.',
+           'Yesterday.'
+       ],
+       correctAnswer: 'Not today.'
+    },
+    {
+        question: 'Who was responsible for the death of Jon Arryn?',
+        answers: [
+            'Varys',
+            'Peter Baelish',
+            'Lysa Arryn',
+            'Eddard Stark'
+        ],
+        correctAnswer: 'Peter Baelish'
+    },
+    {
+        question: 'What was Arya’s sword’s name?',
+        answers: [
+            'Littlefinger',
+            'Crochet',
+            'Pointy end',
+            'Needle'
+        ],
+        correctAnswer: 'Needle'
+    },
+    {
+        question: 'What happened to Arya’s direwolf Nymeria?',
+        answers: [
+            'She went to go live in the wild.',
+            'She was murdered.',
+            'She went to live north of the Wall.',
+            'She stayed in Winterfell.'
+        ],
+        correctAnswer: 'She went to go live in the wild.'
+    },
+    {
+        question: 'Where was Margaery Tyrell from?',
+        answers: [
+            'King\'s Landing',
+            'Winterfell',
+            'Highgarden',
+            'Casterly Rock'
+        ],
+        correctAnswer: 'Highgarden'
+    },
+    {
+        question: 'What was the symbol of House Tyrell?',
+        answers: [
+            'A rose.',
+            'A daffodil.',
+            'A sunflower.',
+            'A daisy.'
+        ],
+        correctAnswer: 'A rose.'
+
     }
-]
+        
+];
